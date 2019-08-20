f0c29faf849527e24f80376067e3edf239022f82
Alejandro Rodarte <alejandrorodarte1@gmail.com>
2019-08-20 09:11:29 -0600

    Lecture 20: Removing a Note

--- notes.js
+++ notes.js
@@ -1,4 +1,5 @@
 const fs = require('fs');
+const chalk = require('chalk');
 
 // method to export
 const getNotes = () => {
@@ -28,11 +29,11 @@ const addNote = (title, body) => {
         // save the new array to the storing file
         saveNotes(notes);
 
-        console.log('Note added...');
+        console.log(chalk.green('Note added...'));
 
     } else {
         // if note existed already, throw a log message and do not add note
-        console.log('Note already exists...');
+        console.log(chalk.red('Note already exists...'));
     }
 
 }
@@ -56,8 +57,34 @@ const loadNotes = () => {
 
 }
 
+// remove a note based on title
+const removeNote = (title) => {
+
+    // get notes
+    const notes = loadNotes();
+
+    // filter out the note that matches the title (if exists)
+    const duplicateNotes = notes.filter((note) => {
+        return title !== note.title;
+    });
+
+    // log message to user if task was successful or not
+    if (notes.length === duplicateNotes.length) {
+        console.log(chalk.red('Note did not exist...'));
+    } else {
+
+        console.log(chalk.green('Note removed...'));
+
+        // save the new altered notes array
+        saveNotes(duplicateNotes);
+        
+    }
+
+}
+
 // use module.exports to export getNotes
 module.exports = {
     getNotes,
-    addNote
+    addNote,
+    removeNote
 };
