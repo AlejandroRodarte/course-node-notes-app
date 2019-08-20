12a4ea0c73ee69ca7120ac24baaa622ba7100b0b
Alejandro Rodarte <alejandrorodarte1@gmail.com>
2019-08-19 21:37:13 -0600

    Lecture 16: Argument Parsing with Yargs: Part I

--- app.js
+++ app.js
@@ -1,26 +1,49 @@
 const chalk = require('chalk');
+const yargs = require('yargs');
 const getNotes = require('./notes');
 
-// first command line argument: action
-const command = process.argv[2];
+// customize the yargs version of your command actions
+yargs.version('1.1.0');
 
-console.log(process.argv);
+// commands register process:
+// 1. write the name of the command
+// 2. write a description for that command
+// 3. setup a handler method when that command is invoked
 
-switch (command) {
+// create the 'add' command
+yargs.command({
+    command: 'add',
+    describe: 'Add a new note',
+    handler: () => {
+        console.log('Adding a new note...');
+    }
+});
 
-    // add a note action
-    case 'add':
-        console.log('Adding note...');
-        break;
-    
-    // remove a note action
-    case 'remove':
-        console.log('Remove note...');
-        break;
-    
-    // unrecognized action
-    default:
-        console.log('Action not recognized');
-        break;
+// create the 'remove' command
+yargs.command({
+    command: 'remove',
+    describe: 'Remove a note',
+    handler: () => {
+        console.log('Removing a note...');
+    }
+});
 
-}
+// create the 'list' command
+yargs.command({
+    command: 'list',
+    describe: 'List all current notes',
+    handler: () => {
+        console.log('Listing all notes...');
+    }
+});
+
+// create the 'red' command
+yargs.command({
+    command: 'read',
+    describe: 'Read a particular note',
+    handler: () => {
+        console.log('Displaying the note...');
+    }
+});
+
+console.log(yargs.argv);
