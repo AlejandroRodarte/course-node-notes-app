188de217f88005fdba1871f7933dd5b4efa7383b
Alejandro Rodarte <alejandrorodarte1@gmail.com>
2019-08-19 19:53:13 -0600

    Lecture 11: Importing NPM Modules

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
+// import the validator npm package found in node_modules folder
+const validator = require('validator');
+
+// log the result of isEmail() method which comes from the validator package
+console.log(validator.isEmail('alejandrorodarte1@gmail.com'));
+
+// now use the isUrl() method
+console.log(validator.isURL('https://www.udemy.com/the-complete-nodejs-developer-course-2/learn/lecture/13728848#questions/7549326'));
+
 // import the getNotes method from the notes.js file
 const getNotes = require('./notes');
-
-// log the result of that method
-console.log(getNotes());
